78d0302686252dd82431f5c57b10b8e7cd63bc42
mina <mina@MINAPC>
2016-04-18 14:25:14 -0400

    Initial Commit

---
+++ js/site.js
@@ -0,0 +1,36 @@
+(function(){
+	"use strict";
+	/* 
+	var $pickbutton = $("#pickbutton");
+	
+	$("#reasonDropdown li a").on("click",function(){
+		var reason = $(this).text();
+		$pickbutton.text(reason);
+	});
+
+	var $sentDialog = $("#sentDialog");
+	
+	$("#contactForm").on("submit",function(){
+		$sentDialog.modal('show');
+		return false;
+	});
+	var $sentAlert = $("#sentAlert");
+	
+	$sentDialog.on("hidden.bs.modal", function(){
+		//alert("close");
+		$sentAlert.show();
+	});
+	$sentAlert.on("close.bs.alert", function(){
+		$sentAlert.hide();
+		return false;
+	});
+	$("contactForm input[type=submit]").tooltip({
+		delay: {
+			show: 500,
+			hide: 0
+		}
+	}); */
+	
+	$("#theCarousel").carousel();
+
+})();
